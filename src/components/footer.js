9903408e1c52d6aa0c5d235bc653a86287945e8a
Ryan Anderson <ryan0425@msn.com>
2021-08-31 19:56:51 -0700

    fixed a few errors in consule regarding logo 192 and className

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,8 +5,8 @@ function footer() {
         <div>
             <div className="jumbotron jumbotron-fluid text-dark jumbotron">
                 <div className="footer-container">
-                    <h1 className="display-4 text-center"> Thanks for stopping by </h1>
-                    <p className="lead text-center"> More projects and updates coming soon!  </p>
+                    <h1 className="display-4 text-center"> Thanks for visiting my portfolio page. </h1>
+                    <p className="lead text-center"> New projects and updates coming soon!  </p>
                 </div>
             </div>
         </div>
