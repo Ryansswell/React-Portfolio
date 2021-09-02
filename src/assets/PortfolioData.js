be3513b773c4ecbe42a3d18324340d0ee580c06e
Ryan Anderson <ryan0425@msn.com>
2021-09-01 22:24:00 -0700

    Added some content to Portfolio page

--- src/assets/PortfolioData.js
+++ src/assets/PortfolioData.js
@@ -2,27 +2,27 @@ const PortfolioArray = [
     {
         "id": 0,
         "imgSrc": "../headshot",
-        "description": "Fitness Tracker!"
+        "description": "Fitness Tracker! Take your fitness training to the next level and stay accountable"
     },
     {
         "id": 1,
         "imgSrc": "/character-mickeymouse.jpeg",
-        "description": "REACT Google Books Search"
+        "description": "REACT Google Books Search: Save and search all books in Google Books"
     },
     {
         "id": 2,
         "imgSrc": "/goldy-spirit.jfif",
-        "description": "User Directory"
+        "description": "User Directory: A unique user directory that you can customize"
     },
     {
         "id": 3,
         "imgSrc": "/goldy-spirit.jfif",
-        "description": "Budget Tracker"
+        "description": "Budget Tracker: Need help making sure you're budget is on track"
     },
     {
         "id": 4,
         "imgSrc": "/goldy-spirit.jfif",
-        "description": "Create ReadMe files"
+        "description": "Create ReadMe files: Save time on creating README files"
     },
     {
         "id": 5,
