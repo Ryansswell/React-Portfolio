42574e436819052f34fc57a858f3d418591b6420
Ryan Anderson <ryan0425@msn.com>
2021-09-15 20:05:16 -0700

    Quick fix on the home.js

--- src/components/home.js
+++ src/components/home.js
@@ -9,7 +9,7 @@ function Home() {
                 <h1>
 
                 </h1>
-                <h3> Full-Stack Web-Developer</h3>
+                <h3> Full-Stack Web Developer</h3>
                 <div className="page-home">
                     <header className="col-lg-auto" >
                         <img className="img-fluid d-block mx-auto" src={IMG_2941} alt="IMG_2941" />
